834a0eab5e087c7d40c40786a551b7f287b47953
Neosvet <neosvet333@gmail.com>
2021-01-27 20:46:00 +0300

    task 3

--- app/src/main/java/ru/neosvet/android1/MainActivity.java
+++ app/src/main/java/ru/neosvet/android1/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView label;
     private EditText text;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         label = findViewById(R.id.label);
         text = findViewById(R.id.text);
+        calendarView = findViewById(R.id.calendarView);
 
         findViewById(R.id.button).setOnClickListener(v -> {
             label.setText(text.getText());
@@ -25,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.openEditAct).setOnClickListener(v -> {
            startActivity(new Intent(MainActivity.this, EditActivity.class));
+        });
+
+        calendarView.setOnDateChangeListener((view, year, month, day) -> {
+            label.setText("new date: " + day + "." + (month+1) + "." + year);
         });
     }
 }
