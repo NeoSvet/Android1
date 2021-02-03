7f5a358e3ada8447d152c46d85c2d791fadc6a96
Neosvet <neosvet333@gmail.com>
2021-02-03 14:10:17 +0300

    refactored code: removed Factory

--- Calculator/src/main/java/ru/neosvet/calculator/Calculator.java
+++ Calculator/src/main/java/ru/neosvet/calculator/Calculator.java
@@ -22,122 +22,46 @@ public class Calculator {
         }
     }
 
-    public static class Factory {
-        private final Calculator calculator = new Calculator();
-        private boolean[] init_state = new boolean[9];
-
-        public Calculator build() throws InstantiationException {
-            if (isReady())
-                return calculator;
-            throw new InstantiationException("Not all views are set-up.");
-        }
-
-        private boolean isReady() {
-            for (int i = 0; i < init_state.length; i++) {
-                if (!init_state[i])
-                    return false;
-            }
-            return true;
-        }
-
-        public Factory setTextView(TextView textView) {
-            init_state[0] = true;
-            calculator.setTextView(textView);
-            return this;
-        }
-
-        public Factory setNumeralButtons(View[] numberButtons) {
-            if (numberButtons.length != 10)
-                return this;
-            init_state[1] = true;
-            calculator.setNumeralButtons(numberButtons);
-            return this;
-        }
-
-        public Factory setBackspaceButton(View button) {
-            init_state[2] = true;
-            calculator.setBackspaceButton(button);
-            return this;
-        }
-
-        public Factory setDotButton(View button) {
-            init_state[3] = true;
-            calculator.setDotButton(button);
-            return this;
-        }
-
-        public Factory setPlusButton(View button) {
-            init_state[4] = true;
-            calculator.setPlusButton(button);
-            return this;
-        }
-
-        public Factory setMinusButton(View button) {
-            init_state[5] = true;
-            calculator.setMinusButton(button);
-            return this;
-        }
-
-        public Factory setMultiplicationButton(View button) {
-            init_state[6] = true;
-            calculator.setMultiplicationButton(button);
-            return this;
-        }
-
-        public Factory setDivisionButton(View button) {
-            init_state[7] = true;
-            calculator.setDivisionButton(button);
-            return this;
-        }
-
-        public Factory setEqualsButton(View button) {
-            init_state[8] = true;
-            calculator.setEqualsButton(button);
-            return this;
-        }
-    } //END FACTORY
-
     private TextView tvResult;
     private boolean lastIsNumeral = false;
     private Actions action = Actions.NONE;
 
-    private Calculator() {
-    }
-
-    private void setTextView(TextView textView) {
+    public Calculator(TextView textView) {
         tvResult = textView;
     }
 
-    private void setEqualsButton(View button) {
-        //TODO setOnClickListener
+    public View.OnClickListener getEqualsEvent() {
+        //TODO OnClickListener
+        return v -> {
+        };
     }
 
-    private void setPlusButton(View button) {
-        button.setOnClickListener( v -> {
+    public View.OnClickListener getPlusEvent() {
+        return v -> {
             action = Actions.PLUS;
             appendAction();
-        });
+        };
     }
 
-    private void setMinusButton(View button) {
-        button.setOnClickListener( v -> {
+    public View.OnClickListener getMinusEvent() {
+        return v -> {
             action = Actions.MINUS;
             appendAction();
-        });
+        };
     }
 
-    private void setDivisionButton(View button) {
-        button.setOnClickListener( v -> {
+    public View.OnClickListener getDivisionEvent() {
+        return v -> {
             action = Actions.DIVISION;
             appendAction();
-        });
+        };
     }
 
-    private void setMultiplicationButton(View button) {
-        button.setOnClickListener( v -> {
+    public View.OnClickListener getMultiplicationEvent() {
+        return v -> {
             action = Actions.MULTIPLICATION;
             appendAction();
-        });
+        };
     }
 
     private void appendAction() {
@@ -147,8 +71,8 @@ public class Calculator {
         lastIsNumeral = false;
     }
 
-    private void setBackspaceButton(View button) {
-        button.setOnClickListener(v -> {
+    public View.OnClickListener getBackspaceEvent() {
+      return v -> {
             if (tvResult.length() == 0)
                 return;
             String s = tvResult.getText().toString();
@@ -156,17 +80,17 @@ public class Calculator {
                 tvResult.setText(s.substring(0, s.length() - 1));
             else
                 tvResult.setText(s.substring(0, s.length() - 2));
-        });
+        };
     }
 
-    private void setDotButton(View button) {
-        //TODO setOnClickListener
+    public View.OnClickListener getDotEvent() {
+        //TODO OnClickListener
+        return v -> {
+        };
     }
 
-    private void setNumeralButtons(View[] numberButtons) {
-        for (int i = 0; i < numberButtons.length; i++) {
-            numberButtons[i].setOnClickListener(numberClickListener);
-        }
+    public View.OnClickListener getNumeralEvent() {
+        return numberClickListener;
     }
 
     private final View.OnClickListener numberClickListener = v -> {
--- Calculator/src/main/java/ru/neosvet/calculator/MainActivity.java
+++ Calculator/src/main/java/ru/neosvet/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package ru.neosvet.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,32 +15,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initCalculator();
+        initNumeralButtons();
+        initOtherButtons();
     }
 
     private void initCalculator() {
-        Calculator.Factory factory = new Calculator.Factory()
-                .setTextView(findViewById(R.id.tvResult))
-                .setNumeralButtons(getNumeralButtons())
-                .setDotButton(findViewById(R.id.bDot))
-                .setBackspaceButton(findViewById(R.id.bBackspace))
-                .setPlusButton(findViewById(R.id.bPlus))
-                .setMinusButton(findViewById(R.id.bMinus))
-                .setMultiplicationButton(findViewById(R.id.bMultiplication))
-                .setDivisionButton(findViewById(R.id.bDivision))
-                .setEqualsButton(findViewById(R.id.bEquals));
-        try {
-            calculator = factory.build();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        }
+        calculator = new Calculator(findViewById(R.id.tvResult));
     }
 
-    private Button[] getNumeralButtons() {
-        return new Button[]{findViewById(R.id.bNumeral0),
+    private void initNumeralButtons() {
+        Button[] buttons = new Button[]{findViewById(R.id.bNumeral0),
                 findViewById(R.id.bNumeral1), findViewById(R.id.bNumeral2), findViewById(R.id.bNumeral3),
                 findViewById(R.id.bNumeral4), findViewById(R.id.bNumeral5), findViewById(R.id.bNumeral6),
                 findViewById(R.id.bNumeral7), findViewById(R.id.bNumeral8), findViewById(R.id.bNumeral9)
         };
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setOnClickListener(calculator.getNumeralEvent());
+        }
+    }
+
+    private void initOtherButtons() {
+        findViewById(R.id.bPlus).setOnClickListener(calculator.getPlusEvent());
+        findViewById(R.id.bMinus).setOnClickListener(calculator.getMinusEvent());
+        findViewById(R.id.bMultiplication).setOnClickListener(calculator.getMultiplicationEvent());
+        findViewById(R.id.bDivision).setOnClickListener(calculator.getDivisionEvent());
+        findViewById(R.id.bDot).setOnClickListener(calculator.getDotEvent());
+        findViewById(R.id.bBackspace).setOnClickListener(calculator.getBackspaceEvent());
+        findViewById(R.id.bEquals).setOnClickListener(calculator.getEqualsEvent());
     }
 
 }
