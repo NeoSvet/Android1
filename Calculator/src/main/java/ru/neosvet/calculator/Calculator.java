1e8303eb7da9093e662b3f767818a6a7128e3d98
Neosvet <neosvet333@gmail.com>
2021-02-03 11:23:33 +0300

    created Calculator.java

---
+++ Calculator/src/main/java/ru/neosvet/calculator/Calculator.java
@@ -0,0 +1,128 @@
+package ru.neosvet.calculator;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class Calculator {
+    enum Actions {
+        NONE, PLUS, MINUS, MULTIPLICATION, DIVISION
+    }
+
+    public static class Factory {
+        private final Calculator calculator = new Calculator();
+        private boolean[] init_state = new boolean[9];
+
+        public Calculator build() throws InstantiationException {
+            if (isReady())
+                return calculator;
+            throw new InstantiationException("Not all views are set-up.");
+        }
+
+        private boolean isReady() {
+            for (int i = 0; i < init_state.length; i++) {
+                if (!init_state[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public Factory setTextView(TextView textView) {
+            init_state[0] = true;
+            calculator.setTextView(textView);
+            return this;
+        }
+
+        public Factory setNumberButtons(View[] numberButtons) {
+            if (numberButtons.length != 10)
+                return this;
+            init_state[1] = true;
+            calculator.setNumberButtons(numberButtons);
+            return this;
+        }
+
+        public Factory setBackspaceButton(View button) {
+            init_state[2] = true;
+            calculator.setBackspaceButton(button);
+            return this;
+        }
+
+        public Factory setDotButton(View button) {
+            init_state[3] = true;
+            calculator.setDotButton(button);
+            return this;
+        }
+
+        public Factory setPlusButton(View button) {
+            init_state[4] = true;
+            calculator.setPlusButton(button);
+            return this;
+        }
+
+        public Factory setMinusButton(View button) {
+            init_state[5] = true;
+            calculator.setMinusButton(button);
+            return this;
+        }
+
+        public Factory setMultiplicationButton(View button) {
+            init_state[6] = true;
+            calculator.setMultiplicationButton(button);
+            return this;
+        }
+
+        public Factory setDivisionButton(View button) {
+            init_state[7] = true;
+            calculator.setDivisionButton(button);
+            return this;
+        }
+
+        public Factory setEqualsButton(View button) {
+            init_state[8] = true;
+            calculator.setEqualsButton(button);
+            return this;
+        }
+    } //END FACTORY
+
+    private TextView tvResult;
+    private boolean lastIsNumber = false;
+    private Actions action = Actions.NONE;
+
+    private Calculator() {
+    }
+
+    private void setTextView(TextView textView) {
+        tvResult = textView;
+    }
+
+    private void setPlusButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setEqualsButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setMinusButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setDivisionButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setMultiplicationButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setBackspaceButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setDotButton(View button) {
+        //TODO setOnClickListener
+    }
+
+    private void setNumberButtons(View[] numberButtons) {
+        //TODO setOnClickListener
+    }
+}
--- Calculator/src/main/java/ru/neosvet/calculator/MainActivity.java
+++ Calculator/src/main/java/ru/neosvet/calculator/MainActivity.java
@@ -3,12 +3,46 @@ package ru.neosvet.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Calculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initCalculator();
     }
+
+    private void initCalculator() {
+        Calculator.Factory factory = new Calculator.Factory()
+                .setTextView(findViewById(R.id.tvResult))
+                .setNumberButtons(getNumberButtons())
+                .setDotButton(findViewById(R.id.bDot))
+                .setBackspaceButton(findViewById(R.id.bBackspace))
+                .setPlusButton(findViewById(R.id.bPlus))
+                .setMinusButton(findViewById(R.id.bMunis))
+                .setMultiplicationButton(findViewById(R.id.bMultiplication))
+                .setDivisionButton(findViewById(R.id.bDivision))
+                .setEqualsButton(findViewById(R.id.bEquals));
+        try {
+            calculator = factory.build();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Button[] getNumberButtons() {
+        return new Button[]{findViewById(R.id.bNumber0),
+                findViewById(R.id.bNumber1), findViewById(R.id.bNumber2), findViewById(R.id.bNumber3),
+                findViewById(R.id.bNumber4), findViewById(R.id.bNumber5), findViewById(R.id.bNumber6),
+                findViewById(R.id.bNumber7), findViewById(R.id.bNumber8), findViewById(R.id.bNumber9)
+        };
+    }
+
 }
