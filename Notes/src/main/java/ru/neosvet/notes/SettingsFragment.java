78d4ac033d316b53c13db690ebadadd262cac943
Neosvet <neosvet333@gmail.com>
2021-03-05 14:20:44 +0300

    realized auth in Google

--- Notes/src/main/java/ru/neosvet/notes/SettingsFragment.java
+++ Notes/src/main/java/ru/neosvet/notes/SettingsFragment.java
@@ -1,17 +1,118 @@
 package ru.neosvet.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 
 public class SettingsFragment extends Fragment {
+    private static final int RC_SIGN_IN = 7;
+    private GoogleSignInClient googleSignInClient;
+    private com.google.android.gms.common.SignInButton btnSignIn;
+    private TextView tvLogin;
+    private MaterialButton btnSignOut;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        initGoogleSign();
+        initView(view);
+        enableSign();
+        return view;
+    }
+
+    private void initGoogleSign() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+    }
+
+    private void initView(View view) {
+        btnSignIn = view.findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        tvLogin = view.findViewById(R.id.tvLogin);
+
+        btnSignOut = view.findViewById(R.id.btnSignOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignInClient.signOut()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                tvLogin.setText("");
+                                enableSign();
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if (account != null)
+            initGoogleAccount(account);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                //https://developers.google.com/identity/sign-in/android/backend-auth?authuser=1
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                initGoogleAccount(account);
+            } catch (ApiException e) {
+                Toast.makeText(requireContext(), R.string.sign_failure, Toast.LENGTH_LONG);
+            }
+        }
+    }
+
+    private void initGoogleAccount(GoogleSignInAccount account) {
+        disableSign();
+        String login = account.getEmail();
+        login = login.substring(0, login.indexOf("@"));
+        tvLogin.setText(login);
+        //TODO: load account.getPhotoUrl() and show in side menu
+    }
+
+    private void enableSign() {
+        btnSignIn.setVisibility(View.VISIBLE);
+        btnSignOut.setVisibility(View.GONE);
+        tvLogin.setVisibility(View.GONE);
+    }
+
+    private void disableSign() {
+        btnSignIn.setVisibility(View.GONE);
+        btnSignOut.setVisibility(View.VISIBLE);
+        tvLogin.setVisibility(View.VISIBLE);
     }
 }
